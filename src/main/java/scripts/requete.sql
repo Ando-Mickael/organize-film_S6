a0dce9616d2cc87cf7b774e008be36658f51aaea
Milclimule <99533207+miclimule@users.noreply.github.com>
2023-03-29 16:28:57 +0300

    i

--- src/main/java/scripts/requete.sql
+++ src/main/java/scripts/requete.sql
@@ -10,6 +10,114 @@ select
     join Plateau p on p.id = s.Plateauid
     group by p.id , p.nom , s.id , s.filmid;
 
+create table IndisponibiliteActeur (
+    id serial primary key,
+    acteurId int references Acteur(id),
+    date date
+);
+
+create table IndisponibilitePlateau (
+    id serial primary key,
+    plateauId int references Plateau(id),
+    date date
+);
+
+CREATE TYPE agendaType AS (
+    IdScene INT,
+    idPlateau INT,
+    plateau varchar(255),
+    duree float4,
+    jour date
+);
+
+CREATE OR REPLACE FUNCTION agendaScene(IDFilm INTEGER , dateDebut date )
+RETURNS SETOF agendaType AS $$
+DECLARE
+    dureeTravaille INT := 480;
+    dureeTotal INT := 0;
+    jour INT := 0;
+    row RECORD;
+BEGIN
+    FOR row IN SELECT Sceneid , plateau, duree , PlateauID FROM listeScene where filmID = IDFilm order by PlateauID LOOP
+
+        FOR row IN SELECT Sceneid , plateau, duree , PlateauID FROM listeScene where filmID = IDFilm order by PlateauID LOOP
+            
+            dureeTotal := dureeTotal + row.duree;
+            if dureeTotal > dureeTravaille then
+                jour := jour + 1;
+                dureeTotal := row.duree;
+            end if;       
+        
+        END LOOP;
+    
+    END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+select * from agendaScene(8);
+
+drop type agendaType cascade; 
+
+
+
+
+CREATE OR REPLACE FUNCTION agendaScene(IDFilm INTEGER, jourPlanifie DATE)
+RETURNS SETOF agendaType AS $$
+DECLARE
+    dureeTravaille INT := 480;
+    dureeTotal INT := 0;
+    jour DATE := jourPlanifie;
+    plateauIndisponible BOOLEAN;
+    dejatraite BOOLEAN;
+    acteurIndisponible BOOLEAN;
+    row RECORD;
+BEGIN
+    FOR row IN SELECT Sceneid, plateau, duree, PlateauID FROM listeScene WHERE filmID = IDFilm ORDER BY PlateauID LOOP
+        plateauIndisponible := EXISTS(SELECT 1 FROM IndisponibilitePlateau WHERE plateauId = row.PlateauID AND date = jour);
+
+        IF plateauIndisponible THEN
+            CONTINUE; -- passe à la prochaine scène
+        END IF;
+
+        FOR row IN SELECT Sceneid , plateau, duree , PlateauID FROM listeScene where filmID = IDFilm order by PlateauID LOOP
+        
+            dureeTotal := dureeTotal + row.duree;
+            if dureeTotal > dureeTravaille then
+                jour := jour + 1;
+                dureeTotal := row.duree;
+            end if;       
+        
+            RETURN NEXT (row.Sceneid, row.PlateauID, row.plateau, row.duree, jour);
+
+        END LOOP;
+
+        
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 CREATE TYPE agendaType AS (
     IdScene INT,
@@ -39,9 +147,3 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
-
-
-select * from agendaScene(8);
-
-drop type agendaType cascade; 
-
